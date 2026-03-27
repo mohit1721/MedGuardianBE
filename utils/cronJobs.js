af8d7368095bbfb0003911c144c39766907ea1b2
Mohit Kumar Mandal <mohitmandal192@gmail.com>
2026-03-27 18:42:04 +0530

    fixing deploy error

--- utils/cronJobs.js
+++ utils/cronJobs.js
@@ -1,20 +1,20 @@
-import cron from "node-cron";
-import moment from "moment-timezone";
-import medicationModel from "../models/medicationModel";
+// const cron = require("node-cron");
+// const Medication = require("../models/medicationModel");
+// const moment = require("moment-timezone");
 
-cron.schedule("0 0 * * *", async () => {
-  try {
-    const today = moment().tz("Asia/Kolkata").startOf("day").toDate();
+// cron.schedule("0 0 * * *", async () => {
+//   try {
+//     const today = moment().tz("Asia/Kolkata").startOf("day").toDate();
     
-    const result = await medicationModel.deleteMany({ expiryDate: { $lt: today } });
+//     const result = await Medication.deleteMany({ expiryDate: { $lt: today } });
 
-    console.log(`Deleted ${result.deletedCount} expired medications.`);
-  } catch (error) {
-    console.error("Error deleting expired medications:", error);
-  }
-}, {
-  scheduled: true,
-  timezone: "Asia/Kolkata"
-});
+//     console.log(`Deleted ${result.deletedCount} expired medications.`);
+//   } catch (error) {
+//     console.error("Error deleting expired medications:", error);
+//   }
+// }, {
+//   scheduled: true,
+//   timezone: "Asia/Kolkata"
+// });
 
-module.exports = {};
+// module.exports = {};
